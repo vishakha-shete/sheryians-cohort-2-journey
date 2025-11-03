d4964df3708032f32c90f614d01409999bea4e3d
vishakha-shete <vishakhashete2@gmail.com>
2025-11-03 23:26:37 +0530

    days code of my learning journey

---
+++ day-15 js/script.js
@@ -0,0 +1,22 @@
+//Assignment operator
+let a = 12;
+a += 13;
+//12 + 12 = 25    
+
+a -= 13;
+//25 - 13 = 12
+
+a *= 2;
+//12 * 2 = 24
+
+a /= 2;
+// 24 / 2 = 12
+
+a %= 2;
+// 12 power 3 = 0
+
+
+//comparison operator
+
+
+
