77a2f724c449a34941bb559799a8752312eae87b
vishakha-shete <vishakhashete2@gmail.com>
2025-11-13 23:45:29 +0530

    completed dom js task

---
+++ dom-js-project1/script.js
@@ -0,0 +1,18 @@
+var increament = document.querySelector('#increament')
+var h1 = document.querySelector('h1')
+var decreament = document.querySelector('#decreament')
+var h2 = document.querySelector('h2')
+
+
+var a = 0;
+increament.addEventListener('click',function(){
+    a++
+    h1.innerHTML = a
+});
+
+
+var b = 0;
+decreament.addEventListener('click',function(){
+    b--
+    h2.innerHTML = b
+});
