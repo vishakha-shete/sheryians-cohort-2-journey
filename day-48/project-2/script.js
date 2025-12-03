5a609f3b0b6a4e10f19d69d4f08b9c18e03ffd15
vishakha-shete <vishakhashete2@gmail.com>
2025-12-03 21:02:36 +0530

    $ built a keyboarb piano project addded^Ciano audio in keywords

--- day-48/project-2/script.js
+++ day-48/project-2/script.js
@@ -1,5 +1,33 @@
+var h1 = document.querySelector('h1');
 
-var h1 = document.querySelector('h1')
-document.body.addEventListener('keydown',function(dets){
-    h1.innerHTML= dets.code
-})
+document.body.addEventListener('keydown', function (event) {
+    let key = event.code;
+    h1.innerHTML = key;
+
+    // Map keyboard keys to notes
+    let noteMap = {
+        "KeyA": "C",
+        "KeyS": "D",
+        "KeyD": "E",
+        "KeyF": "F",
+        "KeyG": "G",
+        "KeyH": "A",
+        "KeyJ": "B",
+        "KeyK": "C",
+        "KeyL": "D",
+        "KeyM": "E",
+        "KeyN": "F",
+        "KeyO": "G",
+    };
+
+    // Check if key pressed is in noteMap
+    if (noteMap[key]) {
+        playSound(noteMap[key]);
+    }
+});
+
+function playSound(note) {
+    let audio = new Audio(`sounds/${note}.mp3`);
+    audio.currentTime = 0;
+    audio.play();
+}
