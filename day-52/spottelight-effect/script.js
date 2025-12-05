baf795c97c4efc5f335bbb33dae689f843756f50
vishakha-shete <vishakhashete2@gmail.com>
2025-12-05 21:03:08 +0530

    creat a spottlight effect

---
+++ day-52/spottelight-effect/script.js
@@ -0,0 +1,8 @@
+addEventListener("mousemove", (e) => {
+    console.log(e.clientX, e.clientY)
+
+
+    document.body.style.setProperty("--x", e.clientX + "px")
+    document.body.style.setProperty("--y", e.clientY + "px")
+
+})
