fab59130d3744ef784c0e63d95a5b4933b4eb189
vishakha-shete <vishakhashete2@gmail.com>
2025-12-09 22:53:16 +0530

    started-lec-day58

---
+++ day-58/script.js
@@ -0,0 +1,10 @@
+// -Introduction to asynchrony in javascript
+// - INtroduction to callbacks and problem in callbacks
+// - Understanding 'promises'-'pendig' 'resolved' 'rejection'
+// - How to Prevent callback hell using 'async' & 'await'.
+// -'setInterval' & 'settimeout' in javascript
+
+
+
+//** */ -Introduction to asynchrony in javascript
+// --->
