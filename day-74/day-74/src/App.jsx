63d04850f6cd3e3c08e893e6b41a24cc26637549
vishakha-shete <vishakhashete2@gmail.com>
2026-01-11 22:38:27 +0530

    learn about form handlling in react

--- day-74/day-74/src/App.jsx
+++ day-74/day-74/src/App.jsx
@@ -1,13 +1,35 @@
 import React,{useState}  from 'react'
 
 const App = () => {
+
+  // 1
   const [num, setNum] = useState(0)
+
+  // 2  
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log('form submittted');
+
+
+  }
   return (
+
+    // 1
     <div>
       <h1>{num}</h1>
       <button onClick={()=>{
         setNum(num + 1)
       }} >increased</button>
+      <div>
+
+      {/* 2 */}
+        <form onSubmit={(e)=>{
+          submitHandler(e)
+          }}>
+          <input type="text" placeholder='enter your name' />
+          <button>submit</button>
+        </form>
+      </div>
     </div>
   )
 }
