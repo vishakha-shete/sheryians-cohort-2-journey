b7d3df8b0e2971bbfd4dcbe0f89a161217813315
vishakha-shete <vishakhashete2@gmail.com>
2026-02-28 23:11:13 +0530

    completed web cam setup

---
+++ day-113/web-cam/Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+const App = () => {
+  return (
+    <div>
+      <FaceExpression/>
+    </div>
+  )
+}
+
+export default App
