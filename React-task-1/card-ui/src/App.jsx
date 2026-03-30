cbdc7b28fb6edee295af2cfd7724b78df4ef71de
vishakha shete <vishakhashete2@gmail.com>
2026-03-30 10:40:59 -0700

    started working on react task card ui

---
+++ React-task-1/card-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='card'>
+      <img className='image' src="https://static.vecteezy.com/system/resources/thumbnails/035/657/675/small_2x/ai-generated-endearing-adventures-with-cute-cartoon-animal-babies-photo.jpg" alt="" />
+      <h3>vishakha shete</h3>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+
+export default App
