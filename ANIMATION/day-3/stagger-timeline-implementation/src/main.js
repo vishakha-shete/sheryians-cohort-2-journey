60f0c6712225d9dfedc0b1e6629579ce7b99a4b0
vishakha shete <vishakhashete2@gmail.com>
2026-06-16 23:32:19 +0530

    feat: implement GSAP stagger, timeline, and position parameter examples

--- ANIMATION/day-3/stagger-timeline-implementation/src/main.js
+++ ANIMATION/day-3/stagger-timeline-implementation/src/main.js
@@ -21,9 +21,9 @@ gsap.to(".box1", {
 
 // timeline implementation
 
-const tl = gsap.timeline()
+const t1 = gsap.timeline()
 
-tl.to(".box2", {
+t1.to(".box2", {
   x: 500,
   duration: 1.3,
   delay: 0.6,
@@ -45,3 +45,55 @@ tl.to(".box2", {
   duration: 1.3,
   ease: "power4.Out",
 })
+
+
+// position parameter implementation
+// "<"= this parameter is used to start the animation of the next element at the same time as the previous animation
+const t2 = gsap.timeline() 
+
+t2.to(".box7", {
+  x: 500,
+  duration: 1.3,
+  delay: 0.6,
+  ease: "power4.Out",
+}).to(".box8", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+},"<"  // start the animation of box8 at the same time as the previous animation (box7)
+).to(".box9", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+}).to( ".box10", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+},"<.08") // start the animation of box10 0.08 seconds after the start of the previous animation (box9)
+
+const t3 = gsap.timeline()
+
+t3.to(".box11", {
+  x: 500,
+  duration: 1.3,
+  delay: 0.6,
+  ease: "power4.Out",
+},"vishakha").to(".box12", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+},"-=0.6") // start the animation of box12 0.4 seconds before the end of the previous animation (box11)
+
+
+const t4 = gsap.timeline()
+
+t4.to(".box13", {
+  x: 500,
+  duration: 1.3,
+  delay: 0.6,
+  ease: "power4.Out",
+},"vishakha").to(".box14", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+  },"+=0.6") // start  the animation of box14 0.6 seconds after the end of the previous animation 
