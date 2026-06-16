602ea8e3804481613a379099c071fc88dee7c7e3
vishakha shete <vishakhashete2@gmail.com>
2026-06-16 20:03:56 +0530

    implemented a timeline animation

--- ANIMATION/day-3/stagger-timeline-implementation/src/main.js
+++ ANIMATION/day-3/stagger-timeline-implementation/src/main.js
@@ -14,21 +14,34 @@ gsap.to(".box1", {
     each: 0.1,
     // from: 'center', // start from center
     // from : 'edges',  // start from edges
-     from: 'random',  // start from random
+    from: 'random',  // start from random
   }
 });
 
 
 // timeline implementation
 
-const tl = gsap.timeline({
-  defaults: {
-    duration: 1.3,
-    ease: "power2.Out",
-  },
-});
+const tl = gsap.timeline()
 
 tl.to(".box2", {
   x: 500,
+  duration: 1.3,
   delay: 0.6,
+  ease: "power4.Out",
+}).to(".box3", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+}).to(".box4", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+}).to(".box5", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
+}).to(".box6", {
+  x: 500,
+  duration: 1.3,
+  ease: "power4.Out",
 })
